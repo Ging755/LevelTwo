3d2e70d3edf5565e888de99384fde20c296e33ee
Gimg755 <denis.milic2@gmail.com>
2019-08-03 00:45:21 +0200

    Setting up archistructure

---
+++ LevelTwo.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LevelTwo.MVC.Startup))]
+namespace LevelTwo.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
