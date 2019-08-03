2a876cabd6b07f0617f3e6a5cb00fea4821b1061
Ging755 <denis.milic2@gmail.com>
2019-08-03 12:46:54 +0200

    Added UnitOfWork and UnitOfWorkFactory

---
+++ LevelTwo.Repository/UOFW/UnitOfWork.cs
@@ -0,0 +1,90 @@
+﻿using LevelTwo.DAL;
+using LevelTwo.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace LevelTwo.Repository.UOFW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected ShopContext DbContext { get; private set; }
+        public UnitOfWork(ShopContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException("DbContext");
+            }
+            DbContext = dbContext;
+        }
+
+        public Task<int> AddAsync<T>(T entity) where T : class
+        {
+            DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+            if (dbEntityEntry.State != EntityState.Detached)
+            {
+                dbEntityEntry.State = EntityState.Added;
+            }
+            else
+            {
+                DbContext.Set<T>().Add(entity);
+            }
+            return Task.FromResult(1);
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            {
+                int result = 0;
+                using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    result = await DbContext.SaveChangesAsync();
+                    scope.Complete();
+                }
+                return result;
+            }
+        }
+
+        public Task<int> DeleteAsync<T>(T entity) where T : class
+        {
+            DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+            if (dbEntityEntry.State != EntityState.Deleted)
+            {
+                dbEntityEntry.State = EntityState.Deleted;
+            }
+            else
+            {
+                DbContext.Set<T>().Attach(entity);
+                DbContext.Set<T>().Remove(entity);
+            }
+            return Task.FromResult(1);
+        }
+
+        public Task<int> DeleteAsync<T>(int id) where T : class
+        {
+            var entity = DbContext.Set<T>().Find(id);
+            if (entity == null)
+            {
+                return Task.FromResult(0);
+            }
+            return DeleteAsync<T>(entity);
+        }
+
+        public Task<int> UpdateAsync<T>(T entity) where T : class
+        {
+            DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+            if (dbEntityEntry.State == EntityState.Detached)
+            {
+                DbContext.Set<T>().Attach(entity);
+            }
+            dbEntityEntry.State = EntityState.Modified;
+
+            return Task.FromResult(1);
+        }
+    }
+}
