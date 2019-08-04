ba1ea4abe3b512985ae1dcea86e030b1ed3b645e
Gimg755 <denis.milic2@gmail.com>
2019-08-04 16:06:36 +0200

    Added Category Repository

---
+++ LevelTwo.Repository/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using LevelTwo.DAL.Entities;
+using LevelTwo.Model.Common;
+using LevelTwo.Repository.Common;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        IGenericRepository<CategoryEntity> repo;
+
+        public CategoryRepository(IGenericRepository<CategoryEntity> repo)
+        {
+            this.repo = repo;
+        }
+        public async Task AddAsync(ICategory entity)
+        {
+           await repo.AddAsync(AutoMapper.Mapper.Map<CategoryEntity>(entity));
+        }
+
+        public async Task DeleteAsync(ICategory entity)
+        {
+            await repo.DeleteAsync(AutoMapper.Mapper.Map<CategoryEntity>(entity));
+        }
+
+        public async Task EditAsync(ICategory entity)
+        {
+            await repo.EditAsync(AutoMapper.Mapper.Map<CategoryEntity>(entity));
+        }
+
+        public async Task<ICategory> GetAsync(int id)
+        {
+            return AutoMapper.Mapper.Map<ICategory>(await repo.GetAsync(id));
+        }
+
+        public IPagedList<ICategory> GetListAsync(int? page, string search)
+        {
+            var categories = AutoMapper.Mapper.Map<IQueryable<ICategory>>(repo.GetListAsync().AsQueryable());
+            if (!string.IsNullOrEmpty(search))
+            {
+                categories = categories.Where(x => x.Name.ToUpper() == search.ToUpper()).AsQueryable();
+            }
+            var model = categories.OrderBy(x => x.Id).ToPagedList((int)page, 3);
+            return (new StaticPagedList<ICategory>(model, model.GetMetaData()));
+        }
+    }
+}
